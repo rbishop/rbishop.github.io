a2ef7a32b93c0a39b097dc9c9d2a09adb2d19071
Richard Bishop <richard@rubiquity.com>
2019-04-04 08:58:59 -0700

    Nginx installed, working script to renew SSL

---
+++ infra/config/TODO.md
@@ -0,0 +1,14 @@
+# Todos
+
+- [x] Redirect HTTP to HTTPS
+- [x] Redirect rubiquity.com to bitsandhops.com
+- [] configure gzip
+- [] configure linux networking settings
+- [] Firewall: only allow 22, 80, 443
+- [] SSL: auto fetch certificate from DNSimple
+- [] SSL: disable SSLv3, TLS 1.0, known bad ciphers
+- [] systemd: Setup firewall rules on init
+- [] systemd: Start nginx
+- [] systemd: Add Nginx logs to journalctl, systemd rotate
+- [] systemd: Run SSL fetch script every so often
+- [] Support HTTP2
---
+++ infra/scripts/get_ssl.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+
+token=$(< dnsimple_token.txt)
+
+log() {
+  local now=$(date +"%Y-%m-%d %H:%M:%S")
+  echo "$now - $1"
+}
+
+if [ -f last_renewed_at.txt ]; then
+  log "Checking for last renewal time"
+  prev_renewal=$(date -f last_renewed_at.txt +"%s")
+else
+  log "Last renewal time not found. Setting to 0"
+  prev_renewal=0
+fi
+
+acc_id=$(curl -sS -H "Authorization: Bearer $token" https://api.dnsimple.com/v2/whoami | jq '.data.account.id')
+
+api_updated_at=$(curl -sS \
+  -H "Authorization: Bearer $token" \
+  https://api.dnsimple.com/v2/$acc_id/domains/bitsandhops.com/certificates \
+| jq --raw-output '.data[] | select(.common_name == "www.bitsandhops.com") | .updated_at')
+
+updated_at=$(date -d $api_updated_at +"%s")
+
+if [ $updated_at -gt $prev_renewal ]; then
+  log "SSL cert has been renewed. Fetching new cert and key"
+  cert_id=$(curl -sS \
+    -H "Authorization: Bearer $token" \
+    https://api.dnsimple.com/v2/$acc_id/domains/bitsandhops.com/certificates \
+  | jq --raw-output '.data[] | select(.common_name == "www.bitsandhops.com") | .id')
+
+
+  log "Writing certificate chain"
+  curl -sS \
+    -H "Authorization: Bearer $token" \
+    https://api.dnsimple.com/v2/$acc_id/domains/bitsandhops.com/certificates/$cert_id/download \
+  | jq --raw-output '.data | .server,.chain[0]' > /var/nginx/secrets/ssl.pem
+
+  log "Writing private key"
+  curl -sS \
+    -H "Authorization: Bearer $token" \
+    https://api.dnsimple.com/v2/$acc_id/domains/bitsandhops.com/certificates/$cert_id/private_key \
+  | jq --raw-output '.data.private_key' > /var/nginx/secrets/ssl.key
+  
+  log "Updating last renewal time to $api_updated_at"
+  echo $api_updated_at > last_renewed_at.txt
+else
+  log "SSL cert has not been renewed. Exiting."
+  exit 0
+fi
